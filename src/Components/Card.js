742dac79527491a72d66fd7b05e5f02f79162696
ViDA_TG <victorademiju.t@gmail.com>
2023-05-10 05:42:33 +0100

    Connect MM

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -7,32 +7,50 @@ export default function Card() {
     const [ task1Active, setTask1Active ] = React.useState(true);
     const [ task2Active, setTask2Active ] = React.useState(false);
     const [ task3Active, setTask3Active ] = React.useState(false);
-    /*
-    function turnOn1(){
-        setSwitch2(false);
-        setSwitch1(true);
+    const [loading, setLoading] = React.useState(false);
+
+    
+    function load(){
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
     }
 
-    function turnOn2(){
-        setSwitch1(false);
-        setSwitch2(true);
+    function done1(){
+        load();
+        setTask1Active(false);
+        setTask2Active(true)
     }
 
-    function generateCode(){
-        setPopupState(true)
+    function done2(){
+        load();
+        setTask2Active(false);
+        setTask3Active(true)
     }
 
+
+    function done3(){
+        load();
+        setTask3Active(false);
+    }
+
+/*
     function closePopup(){
         setPopupState(false);
     }
     */
     return (
         <>
-        <div className="full-card">
-            <div className="loader-container">
-                <div className="spinner"></div>
-            </div>
+        { loading ?
+        <div className="loader-container">
+            <div className="spinner"></div>
+        </div>
+        : "" }
 
+        
+        { loading ? "" :
+        <div className="full-card">
             <div className='card-head'>
                 Complete these tasks
             </div>
@@ -43,7 +61,7 @@ export default function Card() {
                             <img className="in-logo" src={logo} />
                             <span>
                                 Follow Nutz on <a className={ task1Active ? 'task-link' : ''} target='_blank' href={ task1Active ? 'https://google.com' : '#'}>Twitter</a>
-                                <div className={ task1Active ? 'done' : ''}>{ task1Active ? 'DONE' : ''}</div>
+                                <div className={ task1Active ? 'done' : ''} onClick={done1}>{ task1Active ? 'DONE' : ''}</div>
                             </span>
                         </div>
                     </div>
@@ -52,16 +70,16 @@ export default function Card() {
                         <div className="in-task">
                             <img className="in-logo" src={logo} />
                             <span>Like and retweet the Nutz <a className={ task2Active ? 'task-link' : ''} target='_blank' href={ task2Active ? 'https://google.com' : '#'}>pinned post</a>
-                                <div className={ task2Active ? 'done' : ''}>{ task2Active ? 'DONE' : ''}</div>
+                                <div className={ task2Active ? 'done' : ''} onClick={done2}>{ task2Active ? 'DONE' : ''}</div>
                             </span>
                         </div>
                     </div>
 
-                    <div style={ task2Active ? { opacity : '1' } : { opacity : '.2' }}>
+                    <div style={ task3Active ? { opacity : '1' } : { opacity : '.2' }}>
                         <div className="in-task">
                             <img className="in-logo" src={logo} />
                             <span>Make a <a className={ task3Active ? 'task-link' : ''} target='_blank' href={ task3Active ? 'https://google.com' : '#'}> post </a>about Nutz
-                                <div className={ task3Active ? 'done' : ''}>{ task3Active ? 'DONE' : '' }</div>
+                                <div className={ task3Active ? 'done' : ''} onClick={done3}>{ task3Active ? 'DONE' : '' }</div>
                             </span>
                         </div>
                     </div>
@@ -69,10 +87,11 @@ export default function Card() {
             </div>
 
             <div className='card-footer'>
-                <div class="footer-link">Share</div>
-                <div class="footer-link">Claim airdrop</div>
+                <div className="footer-link">Share</div>
+                <div className="footer-link">Claim airdrop</div>
             </div>
         </div>
+        }
         </>
     )
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,10 +2,26 @@ import React from 'react'
 import './header.css'
 
 const Header = () => {
+
+  async function requestAccount() {
+    if(window.ethereum) {
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        })
+        console.log(accounts);
+      } catch (error) {
+        console.log("Error connecting")
+      }
+    } else {
+      console.log("Not detected")
+    }
+  }
+
   return (
     <>
         <div className='header'>
-            <button>CONNECT WALLET</button>
+            <button onClick={requestAccount}>CONNECT WALLET</button>
         </div>
     </>
   )

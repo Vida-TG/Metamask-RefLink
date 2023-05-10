b0d41002affea728d5d73c128751378912dc1665
ViDA_TG <victorademiju.t@gmail.com>
2023-05-10 04:57:16 +0100

    Gif

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './card.css';
+import logo from '../logo.png'
 
 export default function Card() {
 
     const [ task1Active, setTask1Active ] = React.useState(true);
     const [ task2Active, setTask2Active ] = React.useState(false);
     const [ task3Active, setTask3Active ] = React.useState(false);
-    const [ task4Active, setTask4Active ] = React.useState(false);
     /*
     function turnOn1(){
         setSwitch2(false);
@@ -29,23 +29,48 @@ export default function Card() {
     return (
         <>
         <div className="full-card">
-            <div  className='card-body'>
-                <div>
-                    Complete these tasks
-                </div>
-                <div style={ task1Active ? { opacity : '1' } : { opacity : '.4' }}>
-                    <div>
-                        <img />
-                        <span>Follow Nutz on <a _target="blank" href={ task1Active ? 'https://google.com' : '#'}>Twitter</a></span>
-                    </div>
-                </div>
+            <div className="loader-container">
+                <div className="spinner"></div>
+            </div>
 
-                <div style={ task2Active ? { opacity : '1' } : { opacity : '.4' }}>
-                    <div>
-                        <img />
-                        <span>Like and retweet the Nutz <a _target="blank" href={ task2Active ? 'https://google.com' : '#'}>pinned post</a></span>
+            <div className='card-head'>
+                Complete these tasks
+            </div>
+            <div className='card-body'>
+                <div>
+                    <div style={ task1Active ? { opacity : '1' } : { opacity : '.2' }}>
+                        <div className="in-task">
+                            <img className="in-logo" src={logo} />
+                            <span>
+                                Follow Nutz on <a className={ task1Active ? 'task-link' : ''} target='_blank' href={ task1Active ? 'https://google.com' : '#'}>Twitter</a>
+                                <div className={ task1Active ? 'done' : ''}>{ task1Active ? 'DONE' : ''}</div>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div style={ task2Active ? { opacity : '1' } : { opacity : '.2' }}>
+                        <div className="in-task">
+                            <img className="in-logo" src={logo} />
+                            <span>Like and retweet the Nutz <a className={ task2Active ? 'task-link' : ''} target='_blank' href={ task2Active ? 'https://google.com' : '#'}>pinned post</a>
+                                <div className={ task2Active ? 'done' : ''}>{ task2Active ? 'DONE' : ''}</div>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div style={ task2Active ? { opacity : '1' } : { opacity : '.2' }}>
+                        <div className="in-task">
+                            <img className="in-logo" src={logo} />
+                            <span>Make a <a className={ task3Active ? 'task-link' : ''} target='_blank' href={ task3Active ? 'https://google.com' : '#'}> post </a>about Nutz
+                                <div className={ task3Active ? 'done' : ''}>{ task3Active ? 'DONE' : '' }</div>
+                            </span>
+                        </div>
                     </div>
                 </div>
+            </div>
+
+            <div className='card-footer'>
+                <div class="footer-link">Share</div>
+                <div class="footer-link">Claim airdrop</div>
             </div>
         </div>
         </>
